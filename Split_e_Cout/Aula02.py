4b1ff3bba100e36b8ccc8d8aba9e56da7c091eda
Ari Júnior <arioliveirajuniorjose@gmail.com>
2024-02-06 19:43:13 -0300

    add aula02

---
+++ Split_e_Cout/Aula02.py
@@ -0,0 +1,16 @@
+# APRENDENDO A USAR O COUNT PARA CONTAR: LETRAS, PALAVRAS OU CARACTERES
+
+frase01 = "Eu gosto de programar"
+frase02 = "Abacaxi com ameixa para fazer vitamina"
+frase03 = "Precisa comprar: Abacaxi, Ameixa e Leite"
+frase04 = "Camisa-Calça-Moleton-Tênis-Sapatilha"
+
+lista01 = frase01.count("a")  # Conta quantas letras "a" tem na frase (somente minúscula)
+lista02 = frase02.count("a")  # Conta quantas letras "a" tem na frase (somente minúscula)
+lista03 = frase03.count("a")  # Conta quantas letras "a" tem na frase (somente minúscula)
+lista04 = frase04.count("a")  # Conta quantas letras "a" tem na frase (somente minúscula)
+
+print(lista01)
+print(lista02)
+print(lista03)
+print(lista04)
