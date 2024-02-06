92cb1a69ae9c6d3309acc1ae1ecc5054b0bf4ca1
Ari Júnior <arioliveirajuniorjose@gmail.com>
2024-02-06 20:07:57 -0300

    update aula03

--- Split_e_Cout/Aula03.py
+++ Split_e_Cout/Aula03.py
@@ -1,14 +1,14 @@
-# APRENDENDO A USAR O COUNT PARA CONTAR: LETRAS, PALAVRAS OU CARACTERES
+# APRENDENDO A USAR O COUNT E O SPLIT JUNTOS:
 
-frase01 = "amo amoras e o hipopotamo ama pamonhas com ramos de amoreira"
+frase01 = "amo amoras e o hipopotamo gosta de ramos de amoreira"
 
-contagem1 = frase01.count("amo")  # Conta quantas vezes aparece a palacra "amo" na frase
-# PROBLEMA É QUE DESSA FORMA, CONTA ATÉ DENTRO DAS PALAVRAS. 
-print("Contagem: ", contagem1)    # A saída será 6 (mas está errado!)
-
+contagem1 = frase01.count("amo")  # Conta quantas vezes aparece a palavra "amo" na frase.
+# O PROBLEMA É QUE DESSA FORMA, CONTA ATÉ DENTRO DAS PALAVRAS, POR EX. AMOras.
+print("Contagem errada: ", contagem1)    # A saída será 5 (mas está errado!)
+# AMO AMOras e o hipopotAMO gosta de rAMOs de AMOreira
 
 # PARA CORRIGIR ISSO PRECISAMOS SEPARAR A FRASE EM UMA LISTA DE PALAVRAS E VERIFICAR UMA A UMA
 separacao = frase01.split()        # Separando em lista de palavras
 contagem2 = separacao.count("amo") # Contando todas as palavras "amo" na lista
-print("Contagem: ", contagem2)     # Mostrando a contagem correta
+print("Contagem correta: ", contagem2)     # Mostrando a contagem correta
 
