82693165a67da45617dff73855f722f1bd6bfcd7
Ari Júnior <arioliveirajuniorjose@gmail.com>
2024-02-06 19:33:22 -0300

    add aula01

--- Split_e_Cout/Aula01.py
+++ Split_e_Cout/Aula01.py
@@ -1,3 +1,16 @@
+   # APRENDENDO A USAR O SPLIT PARA SEPARAR AS PALAVRAS TRANSFORMANDO EM LISTAS
+
 frase01 = "Eu gosto de programar"
-lista01 = frase01.split()
+frase02 = "Abacaxi com ameixa para fazer vitamina"
+frase03 = "Precisa comprar: Abacaxi, Ameixa e Leite"
+frase04 = "Camisa-Calça-Moleton-Tênis-Sapatilha"
+
+lista01 = frase01.split()    # Separando as palavras pelo espaço (padrão)
+lista02 = frase02.split("a") # Separando pela letra "a" no caso remove apenas em minúsculo
+lista03 = frase03.split(",") # Separando pela visgula
+lista04 = frase04.split("-") # Separando pelo "-"
+
 print(lista01)
+print(lista02)
+print(lista03)
+print(lista04)
