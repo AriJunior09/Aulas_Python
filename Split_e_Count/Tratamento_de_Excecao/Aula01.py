e2b9f5dc59aae359ca9b4d3b725ed273808dcd3e
Ari Júnior <arioliveirajuniorjose@gmail.com>
2024-02-06 21:51:05 -0300

    Update aula01

--- Split_e_Count/Tratamento_de_Excecao/Aula01.py
+++ Split_e_Count/Tratamento_de_Excecao/Aula01.py
@@ -4,8 +4,11 @@ try: # O código crítico que desejamos executar deve estar no escopo do try,
     open("exercicio01.txt")
     print("Arquivo aberto!")
 
-except FileNotFoundError as erro: # Se acontecer esse erro execute essa parte do codigo
+except FileNotFoundError as erro: # Exceção para arquivo não encontrado
     print("O Arquivo não existe ou não foi encontrado!")
     
-except FileExistsError as erro:   # Se acontecer esse erro execute essa parte do codigo
+except FileExistsError as erro:   # Exceção para quando o arquivo ja existe
     print("O Arquivo ou diretório já existe!")
+
+except PermissionError as erro:   # Exceção para falta de permissão
+    print("Não temos permissão para realizar uma operação")
